4902b52df6e82cecf6c932ebd516d189ac73c5e9
codrin2 <hssang127@gmail.com>
2024-09-05 17:48:04 +0900

    [fix] #30 복기노트 저장 API 및 테스트 코드 수정

--- src/main/java/com/letscareer/review/dto/request/ReviewRequest.java
+++ src/main/java/com/letscareer/review/dto/request/ReviewRequest.java
@@ -8,12 +8,12 @@ public record ReviewRequest(
         List<EtcReviewDto> etc  // 기타 리뷰는 별도의 DTO로 처리
 ) {
 
-    public static record ReviewDto(Long id, Long recruitmentId, String satisfaction,
+    public static record ReviewDto(Long id, String satisfaction,
                                    List<String> wellDonePoints, List<String> shortcomingPoints,
                                    String wellDoneMemo, String shortcomingMemo) {
     }
 
-    public static record EtcReviewDto(Long id, Long recruitmentId, String reviewName, String satisfaction,
+    public static record EtcReviewDto(Long id, String reviewName, String satisfaction,
                                       List<String> wellDonePoints, List<String> shortcomingPoints,
                                       String wellDoneMemo, String shortcomingMemo, String difficulty) {
     }
