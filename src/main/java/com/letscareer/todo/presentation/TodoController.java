42a301a5c181dbfea3da9934a44d980f69868b97
codrin2 <hssang127@gmail.com>
2024-09-06 17:25:33 +0900

    [feat] 투두 및 루틴 테스트 코드 작성

--- src/main/java/com/letscareer/todo/presentation/TodoController.java
+++ src/main/java/com/letscareer/todo/presentation/TodoController.java
@@ -47,7 +47,7 @@ public class TodoController {
     }
 
     /**
-     * 해다 기업의 해당 날짜의 투두리스트 반환
+     * 해당 기업의 해당 날짜의 투두리스트 반환
      * @param recruitmentId
      * @param date
      * @return
@@ -58,24 +58,49 @@ public class TodoController {
         return ResponseEntity.ok().body(CommonResponse.ofSuccess("전체 투두 목록을 조회하였습니다.", todoService.getTodosByCompanyName(recruitmentId, date)));
     }
 
+    /**
+     * 투두 수정
+     *
+     * @param todoId  the todo id
+     * @param request the request
+     * @return the response entity
+     */
     @PatchMapping("/todos/{todoId}")
     public ResponseEntity<CommonResponse<Void>> modifyTodo(@PathVariable(name = "todoId") Long todoId, @RequestBody ModifyTodoReq request){
         todoService.modifyTodo(todoId, request);
         return ResponseEntity.ok().body(CommonResponse.ofSuccess("해당 투두를 수정하였습니다.", null));
     }
 
+    /**
+     * 투두 완료 유무 수정
+     *
+     * @param todoId the todo id
+     * @return the response entity
+     */
     @PatchMapping("/todos/{todoId}/check")
     public ResponseEntity<CommonResponse<Void>> modifyTodoIsCompleted(@PathVariable(name = "todoId") Long todoId){
         todoService.modifyTodoIsCompleted(todoId);
         return ResponseEntity.ok().body(CommonResponse.ofSuccess("해당 투두 완료 유무를 수정하였습니다.", null));
     }
 
+    /**
+     * 투두 삭제
+     *
+     * @param todoId the todo id
+     * @return the response entity
+     */
     @DeleteMapping("/todos/{todoId}")
     public ResponseEntity<CommonResponse<Void>> deleteTodo(@PathVariable(name = "todoId") Long todoId){
         todoService.deleteTodo(todoId);
         return ResponseEntity.ok().body(CommonResponse.ofSuccess("해당 투두를 삭제하였습니다.", null));
     }
 
+    /**
+     * 투두 하루 미루기
+     *
+     * @param todoId the todo id
+     * @return the response entity
+     */
     @PatchMapping("/todos/{todoId}/delay")
     public ResponseEntity<CommonResponse<Void>> delayTodo(@PathVariable(name = "todoId") Long todoId){
         todoService.delayTodo(todoId);
